48c377c2bfe8ae9fc71fd76e64db12e18c9d684d
Marek Porębski <marek.bp@poczta.fm>
2022-10-09 18:44:09 +0200

    lab-1.3

--- lab-01/main.js
+++ lab-01/main.js
@@ -1,10 +1,12 @@
 const inputs = document.querySelector('#inputs')
 
-let i = 3 //starting inputs number
 const num1 = document.querySelector('#num1')
 const num2 = document.querySelector('#num2')
 const num3 = document.querySelector('#num3')
-var numbers = []
+let i = 3 //starting inputs number
+// let numbersStr = []
+let numbers = []
+
 
 const resultsDiv = document.querySelector('#results')
 const sumDiv = document.querySelector('#sum')
@@ -18,45 +20,58 @@ const calcBtn = document.querySelector('#calc')
 
 
 calcBtn.addEventListener('click', () => {
-  var sum = 0
-  for (var num of numbers) {
-    sum += (num * 1);
+  let sum = 0
+  for (let num of numbers) {
+    sum += num;
   }
   sumDiv.textContent = `sum: ${sum}`
-  avgDiv.textContent = `avg: ${sum / 3}`
+  avgDiv.textContent = `avg: ${sum / i}`
   minDiv.textContent = `min: ${Math.min.apply(Math, numbers)}`
   maxDiv.textContent = `max: ${Math.max.apply(Math, numbers)}`
 })
 
-// addInputBtn.addEventListener('click', () => {
-//   i++;
-//   let newInput = document.createElement('input')
-//   newInput.setAttribute('id', `num${i}`)
-//   inputs.appendChild(newInput)
-// })
+addInputBtn.addEventListener('click', () => {
+  i++;
+  let newInput = document.createElement('input')
+  newInput.setAttribute('type', 'number')
+  newInput.setAttribute('id', `num${i}`)
+  inputs.appendChild(newInput)
+  numbers.push(+newInput.value)
 
-// removeInputBtn.addEventListener('click', () => {
-//   if (i != 3) {
-//     i--
-//     const removeInput = document.querySelector('input')
-//     if (removeInput.value == '')
-//       inputs.removeChild(inputs.lastChild)
-//   }
-// })
+  const str = 'num'+i
+  const numi = document.querySelector(`#${str}`)
 
+  numi.addEventListener('input', () => {
+    updateNumbers()
+    calcBtn.click();
+  });
+})
 
-num1.addEventListener('change', (event) => {
-  numbers.push(num1.value*1)
+removeInputBtn.addEventListener('click', () => {
+  if (i > 3) {
+    i--
+    inputs.removeChild(inputs.lastChild)
+  }
+})
+
+const updateNumbers = () =>{
+  const temp =  Array.from(document.querySelectorAll('input'))
+  const allNums = temp.map(e=>{return +e.value})
+  numbers = allNums
+}
+
+num1.addEventListener('input', () => {
+  updateNumbers()
   calcBtn.click();
 });
 
-num2.addEventListener('change', (event) => {
-  numbers.push(num2.value*1)
+num2.addEventListener('input', () => {
+  updateNumbers()
   calcBtn.click();
 });
 
-num3.addEventListener('change', (event) => {
-  numbers.push(num3.value*1)
+num3.addEventListener('input', () => {
+  updateNumbers()
   calcBtn.click();
 });
 
